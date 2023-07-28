5b43209090a1fc12b5b2a9b8cbf814071c062f58
Canight322 <62464158+Canight322@users.noreply.github.com>
2023-07-28 17:22:49 +0800

    Add files via upload

---
+++ form/form.js
@@ -0,0 +1,8 @@
+function openPopup(){
+    const popup = document.querySelector('.popup');
+    popup.classList.add('active');
+}
+function closePopup() {
+    const popup = document.querySelector('.popup');
+    popup.classList.remove('active');
+}
